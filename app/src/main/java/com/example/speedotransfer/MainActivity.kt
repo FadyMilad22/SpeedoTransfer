1dde2d883cba7a7633fdcacbc77b7979e808101d
ahmedjamalz <ajamalzayed@gmail.com>
2024-09-01 13:12:22 +0300

    Adding shared UI Elements

--- app/src/main/java/com/example/speedotransfer/MainActivity.kt
+++ app/src/main/java/com/example/speedotransfer/MainActivity.kt
@@ -4,19 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.speedotransfer.ui.theme.SpeedoTransferTheme
 
@@ -27,7 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             SpeedoTransferTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { //innerPadding ->
-
                 }
 //                Login()
             }
