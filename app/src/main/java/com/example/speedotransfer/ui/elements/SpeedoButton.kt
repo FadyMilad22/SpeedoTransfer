88f4a867965f3b061510611988d4239871fa04cc
ahmedjamalz <ajamalzayed@gmail.com>
2024-09-01 13:17:27 +0300

    Adding shared UI Elements

---
+++ app/src/main/java/com/example/speedotransfer/ui/elements/SpeedoButton.kt
@@ -0,0 +1,45 @@
+package com.example.speedotransfer.ui.elements
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.speedotransfer.ui.theme.ButtonTextStyle
+import com.example.speedotransfer.ui.theme.G0
+import com.example.speedotransfer.ui.theme.P300
+
+@Composable
+fun SpeedoButton(text:String,enabled :Boolean,modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 15.dp, bottom = 15.dp)
+            .height(51.dp),
+        border = BorderStroke(1.5.dp, color = P300),
+        shape = RoundedCornerShape(6.dp),
+        enabled = enabled,
+        colors = ButtonColors(
+            disabledContentColor = P300,
+            contentColor = G0,
+            disabledContainerColor = G0,
+            containerColor = if (enabled) P300 else G0
+        )
+    ) {
+        Text(
+            text = text,
+            style = ButtonTextStyle
+        )
+    }
+
+}
+
+//
