1aa601018be693a1749656b2cc0c6106547eb6cc
ahmedjamalz <ajamalzayed@gmail.com>
2024-09-01 08:41:26 +0300

    Adding shared UI Elements

--- app/src/main/java/com/example/speedotransfer/MainActivity.kt
+++ app/src/main/java/com/example/speedotransfer/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.speedotransfer.ui.elements.SpeedoButton
+import com.example.speedotransfer.ui.elements.SpeedoTextField
 import com.example.speedotransfer.ui.theme.SpeedoTransferTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,20 +29,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeedoTransferTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { //innerPadding ->
-
+                Column (verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()){
+                    SpeedoButton(text = "Sign Up", enabled = true)
                 }
+//                Login()
             }
         }
     }
 }
 
 
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpeedoTransferTheme {
-
-    }
-}
--- app/src/main/java/com/example/speedotransfer/ui/elements/SpeedoTextField.kt
+++ app/src/main/java/com/example/speedotransfer/ui/elements/SpeedoTextField.kt
@@ -1,52 +1,129 @@
 package com.example.speedotransfer.ui.elements
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.speedotransfer.R
+import com.example.speedotransfer.ui.theme.BodyRegular14
+import com.example.speedotransfer.ui.theme.BodyRegular16
+import com.example.speedotransfer.ui.theme.D300
+import com.example.speedotransfer.ui.theme.G10
+import com.example.speedotransfer.ui.theme.G70
+import com.example.speedotransfer.ui.theme.G700
 
 
-@Preview
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun SpeedoTextField() {
-    val text = remember {
+fun SpeedoTextField(
+    labelText: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholderText: String,
+    icon: Int,
+    modifier: Modifier = Modifier,
+    keyboardOptions :KeyboardOptions,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    var fullName by remember {
         mutableStateOf("")
     }
-    Column {
+    Column(
+
+    ) {
         Text(
-            text = "Full Name",
-            fontSize = 16.sp,
-            modifier = Modifier.padding(start = 10.dp, bottom = 8.dp)
+            text = labelText,
+            style = BodyRegular16,
+            color = G700,
+            modifier = modifier
+                .fillMaxWidth()
+                .height(24.dp)
+                .padding(start = 16.dp)
         )
-        Spacer(modifier = Modifier.size(10.dp))
         OutlinedTextField(
-            value = text.value, onValueChange = {
-                text.value = it
-            },
-            modifier = Modifier
-                .padding(start = 10.dp),
-            shape = RoundedCornerShape(6.dp),
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = BodyRegular14,
             placeholder = {
-                Text(text = "Enter your Full Name")
-            }, trailingIcon = {
-                Icon(imageVector = Icons.Filled.Build, contentDescription = "")
-            }
+                Text(
+                    text = placeholderText,
+                    style = BodyRegular14,
+                    color = G70,
+                    modifier = modifier
+                        .height(21.dp)
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        painter = painterResource(id = icon), contentDescription = "",
+                        modifier = modifier.size(24.dp)
+                    )
+
+                }
+            },
+
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = G700,
+                focusedBorderColor = G700,
+                focusedTrailingIconColor = G700,
+                cursorColor = G700,
+                disabledPlaceholderColor = G70,
+                disabledTrailingIconColor = G70,
+                disabledBorderColor = G70,
+                errorBorderColor = D300,
+                errorTrailingIconColor = D300
+            ),
+            shape = RoundedCornerShape(6.dp),
+            keyboardOptions = keyboardOptions,
+            visualTransformation = visualTransformation,
+            modifier = modifier
+                .padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+                .fillMaxWidth()
+                .height(52.dp)
 
         )
-
+//        Text(
+//            text = "This is an error message.",
+//            style = BodyRegular14,
+//            color = D300,
+//            modifier = modifier
+//                .fillMaxWidth()
+//                .height(21.dp)
+//                .padding(start = 16.dp)
+//        )
     }
-
 }
+
+
+
+
